c5d19455b8565d9ad7ad0b07b17e2685c7479872
bhaveshpatil856 <bhaveshpatil856@gmail.com>
2020-11-08 16:07:02 +0530

    product

---
+++ schema/products.js
@@ -0,0 +1,42 @@
+let mongoose= require('mongoose');
+let joi= require('joi');
+let Category= require('../schema/categoryModel');
+let SubCategory= require('../schema/subCategoryModel');
+const { date } = require('joi');
+
+let productSchema= mongoose.Schema({
+    name: {type:String,min:5,max:250,required:true},
+    image: {type:String,min:5,max:250,required:true},
+    description: {type:String,min:5,max:250,required:true},
+    price: {type:Number,required:1,minlenght:1},
+    offerPrice: {type:Number,required:1,minlenght:1},
+    isAvailable: {type:Boolean,required:true},
+    isTodayOffer: {type:Boolean,required:true},
+    category:{type: Category.categorySchema, required:true},
+    subCategory: {type:SubCategory.subCatSchema, required:true},
+    recordDate: {type:Date, default:Date.now},
+    updateDate: {type:Date, default:Date.now}
+});
+
+let productModel= mongoose.model('Product',productSchema);
+
+function validateDate(error){
+    let schema= joi.object({
+        name: joi.string().min(5).max(250).required(),
+        image: joi.string().min(5).max(250).required(),
+        description: joi.string().min(5).max(250).required(),
+        price: joi.number().minlenght(1).required(),
+        offerPrice: joi.number().minlenght(1).required(),
+        isAvailable: joi.boolean().required(),
+        isTodayOffer: joi.boolean().required(),
+        category: joi.string().required(),
+        subCategory:  joi.string().required()
+    });
+
+    return schema.validate(error);
+}
+
+module.exports={
+    productModel,productSchema,validateDate
+};
+
